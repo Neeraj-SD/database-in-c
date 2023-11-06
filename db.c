6f40f59e132b22a4d645d47f1bfeb16d54b2bec3
root <root@DESKTOP-7IKENC7.localdomain>
2023-11-06 09:07:06 +0530

    Initial commit

---
+++ db.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+
+    // Load any config files
+
+    // Loop
+    repl(); // READ - EXECUTE - PRINT - LOOP
+
+    // Shutdown operations
+
+    return EXIT_SUCCESS;
+}
